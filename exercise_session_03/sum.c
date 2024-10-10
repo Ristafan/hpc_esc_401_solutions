4f09a1f2fc7ab213bafa91ad4dbe12613b167748
Martin Niels Faehnrich <mfaehnri@eiger-ln004.cscs.ch>
2024-10-11 01:14:12 +0200

    2/3

--- exercise_session_03/sum.c
+++ exercise_session_03/sum.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include "gettime.h"
+#include <omp.h>
 
 typedef struct {
     float x;
@@ -18,8 +20,12 @@ int main(int argc, char *argv[]) {
     
     data = malloc(N * sizeof(coord));
     assert(data);
-    
+
     sum = 0.0;
+
+    double start = getTime();
+
+#pragma omp parallel for reduction(+ : sum)
     for(i=0; i<N; ++i) {
         data[i].x = i & 31;
         data[i].y = i & 63;
@@ -27,6 +33,12 @@ int main(int argc, char *argv[]) {
         data[i].r2 = data[i].x*data[i].x + data[i].y*data[i].y + data[i].z*data[i].z;
         sum += sqrt(data[i].r2);
     }
+
+    double end = getTime();
     printf("sum=%f\n", sum);
+    printf("Computed in %.4g seconds\n", end - start);
+
+    free(data);
+
     return 0;
 }
