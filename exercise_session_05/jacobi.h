1b53c70f0d23bbe37b4bd0f993915f780ef6b02b
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2024-10-24 21:02:03 +0200

    Delete exercise_session_05 directory

--- exercise_session_05/jacobi.h
+++
@@ -1,9 +0,0 @@
-#ifndef JACOBI_H
-#define JACOBI_H
-
-#include "init.h"
-
-double norm_diff(params p, double** mat1, double** mat2);
-void jacobi_step(params p, double** u_new, double** u_old, double** f);
-
-#endif
