9641f2696459fee35ff94c7ebdfdc86af6098e0d
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2024-11-08 14:00:52 +0100

    Add files via upload

---
+++ exercise_session_08/openacc/cpi_openacc.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include "gettime.h"
+
+static long steps = 1000000000;
+
+int main (int argc, const char *argv[]) {
+    int i; double x;
+    double pi;
+    double step = 1.0/(double) steps;
+
+    #pragma acc parallel
+    #pragma acc loop reduction(+:sum) private(x)
+    for (int j=0;j<5;j++){
+        double sum = 0.0;
+        double start = getTime();
+        for (i=0; i < steps; i++) {
+            x = (i+0.5)*step;
+            sum += 4.0 / (1.0+x*x);
+        }
+        pi = step * sum;
+        double delta = getTime() - start;
+        printf("PI = %.16g computed in %.4g seconds\n", pi, delta);
+    }
+}
+
---
+++ exercise_session_08/openacc/gettime.h
@@ -0,0 +1,1 @@
+double getTime(void);
