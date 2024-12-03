cfd1b1de3dfab9167b18a5ffbd6c5e7de5ad8071
Martin Niels Faehnrich <mfaehnri@eiger-ln002.cscs.ch>
2024-12-03 20:28:19 +0100

    ex2 and ex3 added flag

--- exercise_session_11/nbody/nbody.cpp
+++ exercise_session_11/nbody/nbody.cpp
@@ -3,11 +3,10 @@
 using std::vector;
 
 struct particle {
-    float x, y, z;	// position
-    float vx, vy, vz;	// velocity
-    float ax, ay, az;	// acceleration
+	vector<float> x, y, z;	// position
+	vector<float> vx, vy, vz;	//velocity
+	vector<float> ax, ay, az;	//acceleration
 };
-typedef vector<particle> particles;
 
 // Initial conditions
 void ic(particles &plist, int n) {
@@ -15,12 +14,7 @@ void ic(particles &plist, int n) {
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float> dis(0.0, 1.0);
 
-    plist.clear(); 	// Remove all particles
-    plist.reserve(n);	// Make room for "n" particle
-    for( auto i=0; i<n; ++i) {
-	particle p { dis(gen),dis(gen),dis(gen),0,0,0,0,0,0 };
-	plist.push_back(p);
-    }
+// TODO: Implement resize
 
 }
 
@@ -30,18 +24,18 @@ void forces(particles &plist) {
 	float ax=0, ay=0, az=0;
         for(int j=0; j<n; ++j) {			// Depends on all other particles
 	    if (i==j) continue; // Skip self interaction 
-	    auto dx = plist[j].x - plist[i].x;
-	    auto dy = plist[j].y - plist[i].y;
-	    auto dz = plist[j].z - plist[i].z;
+	    auto dx = plist.x[j] - plist.x[i];
+	    auto dy = plist.y[j] - plist.y[i];
+	    auto dz = plist.z[j] - plist.z[i];
 	    auto r = sqrtf(dx*dx + dy*dy + dz*dz);
 	    auto ir3 = 1 / (r*r*r);
 	    ax += dx * ir3;
 	    ay += dy * ir3;
 	    az += dz * ir3;
 	}
-	plist[i].ax = ax;
-	plist[i].ay = ay;
-	plist[i].az = az;
+	plist.ax[i] = ax;
+	plist.ay[i] = ay;
+	plist.az[i] = az;
     }
 }
 
