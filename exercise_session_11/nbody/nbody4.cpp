42ba4c9a523c31785dff1a0af63358c9d73d26ac
Martin Niels Faehnrich <mfaehnri@eiger-ln002.cscs.ch>
2024-12-03 21:21:33 +0100

    ex4

---
+++ exercise_session_11/nbody/nbody4.cpp
@@ -0,0 +1,62 @@
+#include <random>
+#include <vector>
+using std::vector;
+
+struct particle {
+    float x, y, z;	// position
+    float vx, vy, vz;	// velocity
+    float ax, ay, az;	// acceleration
+};
+typedef vector<particle> particles;
+
+// Initial conditions
+void ic(particles &plist, int n) {
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<float> dis(0.0, 1.0);
+
+    
+    for( auto i=0; i<n; ++i) {
+	particle p { dis(gen),dis(gen),dis(gen),0,0,0,0,0,0 };
+    }
+
+}
+
+void forces(particles &plist) {
+    int n = plist.size();
+    for(int i=0; i<n; ++i) { 		// We want to calculate the force on all particles
+	float ax=0, ay=0, az=0;
+        for(int j=0; j<i; ++j) {			// Depends on all other particles 
+	    auto dx = plist[j].x - plist[i].x;
+	    auto dy = plist[j].y - plist[i].y;
+	    auto dz = plist[j].z - plist[i].z;
+	    auto r = sqrt(dx*dx + dy*dy + dz*dz);
+	    auto ir3 = 1 / (r*r*r);
+	    ax += dx * ir3;
+	    ay += dy * ir3;
+	    az += dz * ir3;
+	}
+	for(int j=i+1; j<n; ++j) {                        // Depends on all other particles
+            auto dx = plist[j].x - plist[i].x;
+            auto dy = plist[j].y - plist[i].y;
+            auto dz = plist[j].z - plist[i].z;
+            auto r = sqrt(dx*dx + dy*dy + dz*dz);
+            auto ir3 = 1 / (r*r*r);
+            ax += dx * ir3;
+            ay += dy * ir3;
+            az += dz * ir3;
+        }
+	plist[i].ax = ax;
+	plist[i].ay = ay;
+	plist[i].az = az;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int N=50'000;
+    particles plist;
+    ic(plist,N);
+    forces(plist);
+    return 0;
+}
+
